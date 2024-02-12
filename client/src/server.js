e19e9b2c574369c2952349b12c510a992a772871
venasven <honhonlem@gmail.com>
2024-02-12 04:25:08 -0800

    moved file

--- client/src/server.js
+++ client/src/server.js
@@ -7,6 +7,6 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require('../../server/routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
